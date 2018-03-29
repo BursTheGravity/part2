d31090903c0be9f3eef63cccf34f8859cc199c5e
Solepride <leman.yan80@myhunter.cuny.edu>
2018-03-29 13:41:16 -0400

    comments

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 -The program compiles and works as specified. 
 -However does not check if the other programs work individually
 -Nice structure and indentation
--Rename a couple of variables that could have had a better name like ans could be counter
+-Rename a couple of variables with a better name like ans could be counter, when counting the occurences of a character
 -I am going to try to add a feature that supports for loops and if statements that don’t have curly braces
 -if the line has an if and a for in the line, then the amount of tabs should increase by one
--How do we know when the loop statement ends and we no longer needs the tabs. 
+-How do we know when the loop statement ends when there is no closed bracket and we no longer needs the tabs. 
 
--- main.cpp
+++ main.cpp
@@ -8,29 +8,31 @@ using std::endl;
 
 
 int main() {
-	
+
+	cout<<removeLeadingSpaces("   int x=0;")<<endl;
+
+	cout<<countChar("abcd {} }}", '}')<<endl;
+
 	string line, ans;			//Had a variable l that was not used. 
-	
+
 	int numOpen, numClosed;			//Num of open brackets and num of closed brackets
 	
-	while ( getline(cin,line) ) {				
-		int numTabs = numOpen - numClosed;
+	while ( getline(cin,line) ) {			//Read the lines		
+		int numTabs = numOpen - numClosed;	//Number of open brackets minus number of closed brackets results in the amount of tabs needed
 		string tabs = "";
 
-		string cleanLine = removeLeadingSpaces(line);
-		if (cleanLine[0] == '}') { numTabs -= 1; }
+		string cleanLine = removeLeadingSpaces(line);	//Remove the spaces from each line
+		if (cleanLine[0] == '}') { numTabs -= 1; }	//If the first char is a closing bracket then the number of tabs decreases by one
 		
 
-		for (int i = 0; i < numTabs; i++ ) {
+		for (int i = 0; i < numTabs; i++ ) {		//Add the number of tabs
 			tabs += "\t";
 		}
 
+		ans += tabs + cleanLine + "\n";			//Add the tabs 
 		
-		
-		ans += tabs + cleanLine + "\n";
-		
-		numOpen += countChar(line, '{');
-		numClosed += countChar(line, '}');
+		numOpen += countChar(line, '{');		//Number of open brackets 
+		numClosed += countChar(line, '}');		//Number of closed brackets
 	}
 	
 	cout << ans << endl;
