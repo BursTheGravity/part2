d0fc6e8ae3b18f1813fd8d6919388345217b5426
Solepride <leman.yan80@myhunter.cuny.edu>
2018-03-29 10:58:07 -0400

    Added comments to leo's lab

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # part2
+
+-The program compiles and works as specified. 
+-However does not check if the other programs work individually
+-Nice structure and indentation
+-Rename a couple of variables that could have had a better name like ans could be counter
+-I am going to try to add a feature that supports for loops and if statements that don’t have curly braces
+-if the line has an if and a for in the line, then the amount of tabs should increase by one
+-How do we know when the loop statement ends and we no longer needs the tabs. 
+
--- main.cpp
+++ main.cpp
@@ -9,19 +9,24 @@ using std::endl;
 
 int main() {
 	
-	string line, ans, l;
+	string line, ans;			//Had a variable l that was not used. 
 	
-	int numOpen, numClosed;
+	int numOpen, numClosed;			//Num of open brackets and num of closed brackets
 	
-	while ( getline(cin,line) ) {
+	while ( getline(cin,line) ) {				
 		int numTabs = numOpen - numClosed;
 		string tabs = "";
+
 		string cleanLine = removeLeadingSpaces(line);
 		if (cleanLine[0] == '}') { numTabs -= 1; }
 		
+
 		for (int i = 0; i < numTabs; i++ ) {
 			tabs += "\t";
 		}
+
+		
+		
 		ans += tabs + cleanLine + "\n";
 		
 		numOpen += countChar(line, '{');
